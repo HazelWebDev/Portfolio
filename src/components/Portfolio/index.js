6399316c642611d0b0e6a23d1dd166636818189f
Kristen Roggero <kristenroggero@Kristens-Air.attlocal.net>
2023-07-02 13:29:50 -0500

    updated portfolio portion

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -26,78 +26,106 @@ const Portfolio = () => {
             idx={15}
           />
         </h1>
-        <div className="full">
-          <div className="left">
+        <div className="container portfolio">
+          <div className="leftside">
             <div className="image">
-              <img
-                src="https://media.geeksforgeeks.org/wp-content/uploads/20220202083519/gfglogo.png"
-                alt="gfg-logo"
-              />
+              <img src="img" alt="gfg-logo" />
             </div>
             <div className="Contact">
-              <h2>Contact</h2>
+              <h2 className="contactTitle">Contact Me</h2>
               <p>
-                <b>Email id:</b>xyz@gmail.com
+                <b>Email: </b> @gmail.com
               </p>
               <p>
-                <b>Mobile no :</b>1234567890
+                <b>Mobile: </b> 111
               </p>
             </div>
-            <div className="Skills">
-              <h2>Skills</h2>
+            <div className="education">
+              <h2>Education</h2>
+              <table>
+                <tr>
+                  <th>University </th>
+                  <th> Duration</th>
+                </tr>
+                <tr>
+                  <td>xyz</td>
+                  <td>2020</td>
+                </tr>
+              </table>
+            </div>
+            <div className="skills">
+              <h2 className="skillTitle"> Technical Skills</h2>
               <ul>
-                <li>
-                  <b>Programming Languages : Python, Java, C++</b>
-                </li>
-                <li>
-                  <b>Frontend : HTML5, CSS3, JavaScript, React</b>
-                </li>
-                <li>
-                  <b>Backend : python Node.js</b>
-                </li>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JavaScript</li>
+                <li>React</li>
+                <li>Java</li>
               </ul>
-            </div>
-            <div className="Language">
-              <h2>Language</h2>
+              <h2>Soft Skills</h2>
               <ul>
+                <li>Communication</li>
+                <li>Collaboration</li>
+                <li>Problem Solving</li>
+                <li>Time Management</li>
+                <li>Multitasking</li>
                 <li>English</li>
-                <li>Hind di</li>
+              </ul>
+              <h2>Certifications</h2>
+              <ul>
+                <li>Certified Web Developer - freeCodeCamp</li>
+                <li>codeCademy</li>
               </ul>
             </div>
             <div className="Hobbies">
               <h2>Hobbies</h2>
               <ul>
-                <li>Playing cricket</li>
+                <li>Playing chess</li>
+                <li>Crocheting</li>
+                <li>Reading</li>
                 <li>Swimming</li>
               </ul>
             </div>
           </div>
-          <div className="right">
-            <div className="name">
-              <h1>GeeksforGeeks</h1>
-            </div>
+          <div className="rightside">
+            <h1 className="name">Kristen Roggero</h1>
             <div className="title">
-              <p>Web Developer</p>
+              <h5>Web Developer</h5>
             </div>
-            <div className="Summary">
-              <h2>Summary</h2>
+            <div className="aboutMe">
+              <h3>About Me</h3>
               <p>
-                To secure a challenging position in a reputable organization to
-                expand my learning knowledge and skill
+                My goal as a developer is to create remarkable and inspiring
+                user interfaces.
               </p>
             </div>
-            <div className="Experience">
-              <h2>Experience</h2>
-              <h3>Abc webdev pvt ltd - Senior Web Developer</h3>
-              <p>January 2022 to Present</p>
+            <div className="experiences">
+              <h3 className="experience">Experience</h3>
+              <h4>Infosys - Software Engineer</h4>
+              <p className="duration">August 2021 to December 2021</p>
+              <p>Roles and Responsibilities... </p>
               <ul>
                 <li>
                   Actively engaged in web creative design and development.
                 </li>
                 <li>Designing project & planning</li>
+                <li>Working on designing</li>
               </ul>
-              <h3>Xyz webdev pvt ltd - junior web developer</h3>
-              <p>August 2021 to December 2021</p>
+              <br />
+              <h4>Revature - Software Engineer</h4>
+              <p className="duration">August 2021 to December 2021</p>
+              <p>Roles and Responsibilities... </p>
+              <ul>
+                <li>
+                  Actively engaged in web creative design and development.
+                </li>
+                <li>Designing project & planning</li>
+                <li>Working on designing</li>
+              </ul>
+              <br />
+              <h4>Merced County Library - Library Resource Technician</h4>
+              <p className="duration">August 2021 to December 2021</p>
+              <p>Roles and Responsibilities... </p>
               <ul>
                 <li>
                   Actively engaged in web creative design and development.
@@ -106,36 +134,19 @@ const Portfolio = () => {
                 <li>Working on designing</li>
               </ul>
             </div>
-            <div className="Education">
-              <h2>Education</h2>
-              <table>
-                <tr>
-                  <th>University/college </th>
-                  <th>Passing year </th>
-                  <th>percentage/cgpa</th>
-                </tr>
-                <tr>
-                  <td>xyz</td>
-                  <td>2020</td>
-                  <td>8.9</td>
-                </tr>
-                <tr>
-                  <td>pqr</td>
-                  <td>2018</td>
-                  <td>89%</td>
-                </tr>
-              </table>
-            </div>
             <div className="project">
+              <h3 className="myProjects">Projects I've Done</h3>
+              <h4>Project1</h4>
               <ul>
-                <li>
-                  <h2>Project1</h2>
-                  <p>This project is based on html & used React</p>
-                </li>
-                <li>
-                  <h2>Project2</h2>
-                  <p>This project is based on html & used React</p>
-                </li>
+                <li>This project is based on html & used React</li>
+              </ul>
+              <h4>Project2</h4>
+              <ul>
+                <li>This project is based on html & used React </li>
+              </ul>
+              <h4>Project3</h4>
+              <ul>
+                <li>This project is based on html & used React </li>
               </ul>
             </div>
           </div>
